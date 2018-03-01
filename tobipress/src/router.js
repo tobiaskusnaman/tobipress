7d8224000e7eca87113ab5621746bb952f4eed6f
tobiaskusnaman <tobiaskusnaman@gmail.com>
2018-03-01 14:06:09 +0700

    first check point

--- tobipress/src/router.js
+++ tobipress/src/router.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 import About from './views/About.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: Login
     },
     {
       path: '/about',
